514726242e4b64886068474878261f4bbb2e807b
pl1nk <adeellq8@gmail.com>
2023-09-06 19:28:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 # write your code here
+person = {
+
+    "name": "Abdullah",
+    "age": 27,
+    "hobbies": ['Gaming', 'Coding','fixing cars' ]
+
+
+
+}
+
+print(person["name"])
+print(person["age"])
+
+person.update({'age':19})
+person['country'] = 'Kuwait'
+print(person)
+print(len(person))
+
+person["hobbies"].append("food") 
+
+def check_hobbies(person):
+    if len(person["hobbies"]) > 3 :
+        print("WOW YOU ARE AMAZING!!!")
+    
+
+check_hobbies(person)            
